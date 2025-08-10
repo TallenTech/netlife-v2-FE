cb35c5d05aca2d7baea48296262075848c5be2c8
Daniel <dtusingwire17@gmail.com>
2025-08-10 10:13:15 +0300

    set the project

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ai11hh7hvg"
+    "revision": "0.6jga7rbbnfo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
