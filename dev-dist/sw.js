ab94de3809cb2bc56d339240b881a9256645d9d6
Daniel <dtusingwire17@gmail.com>
2025-08-14 12:44:55 +0300

    profile step, header improvments

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.kt1287khp1g"
+    "revision": "0.2b77ai194q"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
