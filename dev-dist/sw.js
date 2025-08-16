76c94e7795b6e8d2250a1ab6826676479a7e632c
Daniel <dtusingwire17@gmail.com>
2025-08-16 12:11:14 +0300

    .env back

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.aj7c2qdi3h"
+    "revision": "0.5oqlag5q28g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
