e39e3cf624950e18c2df359a1c816e33ad642034
musatallen <musa@tallenhub.com>
2025-08-17 11:54:31 +0300

    fix imports

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.dicltf7a3rg"
+    "revision": "0.agatu3r7esg"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
