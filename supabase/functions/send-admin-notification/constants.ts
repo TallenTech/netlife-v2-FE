e4c705b02cd5da6afaf8847274372ea0be21bc3d
musatallen <musa@tallenhub.com>
2025-08-17 12:30:08 +0300

    add supaabse functions

--- supabase/functions/send-admin-notification/constants.ts
+++ supabase/functions/send-admin-notification/constants.ts
@@ -3,7 +3,7 @@ export const FROM_ADDRESS = "life@netlife.cc";
 export const ADMIN_LIST = [
   { name: "Tallen", email: "tallen@tallen.tech" },
   { name: "Musa", email: "musa@tallenhub.com" },
-  { name: "Mooya", email: "mooya@tallenhub.com" },
+  //   { name: "Mooya", email: "mooya@tallenhub.com" },
   { name: "Danniel", email: "Td117235@gmail.com" },
 ];
 
