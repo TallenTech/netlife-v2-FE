01559bb98dbeffc1840b8d354389b1ace3261590
musatallen <musa@tallenhub.com>
2025-08-19 17:29:01 +0300

    update app download

--- src/hooks/usePWAInstall.js
+++ src/hooks/usePWAInstall.js
@@ -1,31 +1,73 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const usePWAInstall = () => {
-  const [installPrompt, setInstallPrompt] = useState(null);
+  const [installPromptEvent, setInstallPromptEvent] = useState(null);
+  const [canInstall, setCanInstall] = useState(false);
+
+  const isIOS = () => {
+    if (typeof window === "undefined") return false;
+    return /iPad|iPhone|iPod/.test(navigator.userAgent) && !window.MSStream;
+  };
+  const isStandalone = () => {
+    if (typeof window === "undefined") return false;
+    return (
+      window.matchMedia("(display-mode: standalone)").matches ||
+      window.navigator.standalone === true
+    );
+  };
 
   useEffect(() => {
-    const handleBeforeInstallPrompt = (e) => {
-      e.preventDefault();
-      setInstallPrompt(e);
+    const handleBeforeInstallPrompt = (event) => {
+      event.preventDefault();
+      setInstallPromptEvent(event);
+      setCanInstall(true);
+      console.log("PWA install prompt is available.");
     };
 
     window.addEventListener("beforeinstallprompt", handleBeforeInstallPrompt);
+
+    const handleAppInstalled = () => {
+      console.log("PWA was installed");
+      setInstallPromptEvent(null);
+      setCanInstall(false);
+    };
+
+    window.addEventListener("appinstalled", handleAppInstalled);
 
     return () => {
       window.removeEventListener(
         "beforeinstallprompt",
         handleBeforeInstallPrompt
       );
+      window.removeEventListener("appinstalled", handleAppInstalled);
     };
   }, []);
 
-  const handleInstallClick = async () => {
-    if (!installPrompt) {
+  const handleInstallClick = useCallback(async () => {
+    if (!installPromptEvent) {
+      console.log("Install prompt not available.");
       return;
     }
-    await installPrompt.prompt();
-    setInstallPrompt(null);
-  };
 
-  return { canInstall: !!installPrompt, handleInstallClick };
+    installPromptEvent.prompt();
+
+    const { outcome } = await installPromptEvent.userChoice;
+
+    console.log(`User response to the install prompt: ${outcome}`);
+    setInstallPromptEvent(null);
+    setCanInstall(false);
+
+    if (outcome === "accepted") {
+      setTimeout(() => {
+        window.open(window.location.origin, "_self");
+      }, 1000);
+    }
+  }, [installPromptEvent]);
+
+  return {
+    canInstall,
+    handleInstallClick,
+    isIOS: isIOS(),
+    isStandalone: isStandalone(),
+  };
 };
